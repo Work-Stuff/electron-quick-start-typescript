cb0b801e5832b324517525c4efeebca55d42cda8
Tom Needham <06needhamt@gmail.com>
2019-07-01 08:19:52 +0100

    Add WebRTC Connection Functions

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -38,4 +38,57 @@ export class ManagePeer {
             });
         };
     }
+
+    static typeOfSignal(signalType: SignalingType) {
+        return SignalingType[signalType];
+    }
+
+    public addTrack(track: MediaStreamTrack) {
+        if (track instanceof VideoTrack) {
+            this.videoTrack = track as VideoTrack;
+            this.peerConnection.addTrack(track);
+        }
+        else if (track instanceof AudioTrack) {
+            this.audioTrack = track as AudioTrack;
+            this.peerConnection.addTrack(track);
+        }
+        else {
+            console.log("Unkown Stream Type {0}", typeof (track));
+        }
+    }
+
+    public connect() {
+        this.peerConnection.createOffer()
+            .then(offer => {
+                this.peerConnection.setLocalDescription(offer).then(() => {
+                    this.socket.emit('signaling', {
+                        to: this.userSocketId,
+                        from: this.socket.id,
+                        type: SignalingType.Offer,
+                        data: offer
+                    });
+                })
+                    .catch(err => console.error(err));
+            })
+            .catch((error) => {
+                console.error('error', error);
+            });
+    }
+
+    public addConnection(offer: any) {
+        this.peerConnection.setRemoteDescription(new RTCSessionDescription(offer.data))
+            .then(() => {
+                return this.peerConnection.createAnswer();
+            })
+            .then(answer => {
+                this.peerConnection.setLocalDescription(answer).then(() => {
+                    this.socket.emit('signaling', {
+                        to: offer.from,
+                        from: this.socket.id,
+                        type: SignalingType.Answer,
+                        data: answer
+                    });
+                }).catch(err => console.error(err));
+            });
+    }
 }
